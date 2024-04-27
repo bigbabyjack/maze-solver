33c32b5b9dff31d0e5df11157702fda8de45032f
Jack Conner <1jackconner@gmail.com>
2024-04-27 16:39:36 -0700

    initial commit

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from window import Window
+from line import Line, Point
 
 
 def main():
     win = Window(800, 600)
+    p1 = Point(0, 0)
+    p2 = Point(100, 100)
+    line = Line(p1=p1, p2=p2)
+    win.draw_line(line=line, fill_color="red")
     win.wait_for_close()
 
 
--- window.py
+++ window.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+from line import Line
 
 
 class Window:
@@ -22,3 +23,6 @@ class Window:
     def close(self):
         self.is_running = False
         self.__root = Tk()
+
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(canvas=self.canvas, fill_color=fill_color)
