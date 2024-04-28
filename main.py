65c66ddb8c6d603108882509b8ba114a75c4be9a
Jack Conner <1jackconner@gmail.com>
2024-04-27 17:29:43 -0700

    add cells

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from window import Window
-from line import Line, Point
+from cell import Cell
 
 
 def main():
     win = Window(800, 600)
-    p1 = Point(0, 0)
-    p2 = Point(100, 100)
-    line = Line(p1=p1, p2=p2)
-    win.draw_line(line=line, fill_color="red")
+
+    c1 = Cell(win)
+    c1.has_left_wall = False
+    c1.draw(50, 50, 100, 100)
+
+    c2 = Cell(win)
+    c2.has_right_wall = False
+    c2.draw(120, 120, 250, 250)
+
+    c1.draw_move(c2)
+
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.draw(300, 300, 800, 800)
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_bottom_wall = False
+    c4.draw(500, 500, 200, 200)
     win.wait_for_close()
+
+    c3.draw_move(c4)
 
 
 main()
